d52444f20c2eab11dfa69a002d07f52786299e47
natinei01 <47499303+natinei01@users.noreply.github.com>
2019-02-10 20:58:17 +0700

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            int tex;
+            int anwser;
+            Console.Write("Input your PayMount:");
+            int.TryParse(Console.ReadLine(), out result);
+            Console.Write("Input your Tex peryear:");
+            int.TryParse(Console.ReadLine(), out tex);
+            anwser = result * tex / 100;
+            Console.WriteLine($"your tex peryear={anwser}");                                   
+            Console.ReadKey();
+                
+        }
+    }
+}
